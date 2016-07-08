ee51a3995574157acc94320e36585d1f1c07184e
bicossampalis <beaucoupfish25@hotmail.com>
2016-07-08 11:46:32 +0300

    Version 2.0
    Adding iOS connectity

---
+++ src/ios/com/zebraprint/epsilonnet/MfiBtPrinterConnection.m
@@ -0,0 +1,29 @@
+#import <ExternalAccessory/ExternalAccessory.h>
+ #import "MfiBtPrinterConnection.h"
+ -(void)sendZplOverBluetooth:(CDVInvokedUrlCommand*)command{
+ 
+     NSString *serialNumber = @[command.arguments objectAtIndex:0];
+     
+     // Instantiate connection to Zebra Bluetooth accessory
+     id<ZebraPrinterConnection, NSObject> thePrinterConn = [[MfiBtPrinterConnection alloc] initWithSerialNumber:serialNumber];
+     
+     // Open the connection - physical connection is established here.
+     BOOL success = [thePrinterConn open];
+     
+     // This example prints "This is a ZPL test." near the top of the label.
+     NSString *zplData = @[command.arguments objectAtIndex:1];
+     
+     NSError *error = nil;
+     // Send the data to printer as a byte array.
+     success = success && [thePrinterConn write:[zplData dataUsingEncoding:NSUTF8StringEncoding] error:&error];
+     
+     if (success != YES || error != nil) {
+         UIAlertView *errorAlert = [[UIAlertView alloc] initWithTitle:@"Error" message:[error localizedDescription] delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+         [errorAlert show];
+         [errorAlert release];
+     }
+     // Close the connection to release resources.
+     [thePrinterConn close];
+     
+     [thePrinterConn release];
+ }
