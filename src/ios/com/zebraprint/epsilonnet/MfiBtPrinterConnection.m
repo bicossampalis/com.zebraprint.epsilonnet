4d08e937f9647e1c8112afdf9d983837a2501d59
bicossampalis <beaucoupfish25@hotmail.com>
2016-07-13 18:15:08 +0300

    2.52

--- src/ios/com/zebraprint/epsilonnet/MfiBtPrinterConnection.m
+++ src/ios/com/zebraprint/epsilonnet/MfiBtPrinterConnection.m
@@ -1,18 +1,27 @@
 #import <ExternalAccessory/ExternalAccessory.h>
 #import "MfiBtPrinterConnection.h"
  
-  @implementation MfiBtPrinterConnection
-  
- +(NSString*)sendZplOverBluetoothParent:(NSArray*)command{
++(NSString*)sendZplOverBluetoothParent:(NSArray*)command{
  
      NSString* serialNumber = command[0];
     
-    id<ZebraPrinterConnection, NSObject> thePrinterConn = [[MfiBtPrinterConnection alloc] initWithSerialNumber:serialNumber];
- 
+
+     id<ZebraPrinterConnection, NSObject> thePrinterConn = [[MfiBtPrinterConnection alloc] initWithSerialNumber:serialNumber];
      
+     
+     BOOL success = [thePrinterConn open];
+     
+     
+     NSString* zplData = command[1];
+     
+     NSError* error = nil;
+     
+     success = success && [thePrinterConn write:[zplData dataUsingEncoding:NSUTF8StringEncoding] error:&error];
+  
+     
+     [thePrinterConn close];
      
 	 
 	  return serialNumber;
  }
  
- @end
