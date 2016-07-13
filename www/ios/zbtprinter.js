4af29a0965d21e8c2525097e7225d0901f07c92c
bicossampalis <beaucoupfish25@hotmail.com>
2016-07-13 12:22:16 +0300

    2.32

--- www/ios/zbtprinter.js
+++ www/ios/zbtprinter.js
@@ -1,9 +1,9 @@
 var exec = require('cordova/exec');
 
 exports.print = function(mac, str, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'EpsilonPrinter', 'sendZplOverBluetooth', [mac, str]);
+    cordova.exec(successCallback, errorCallback, 'ZebraBluetoothPrinter', 'sendZplOverBluetooth', [mac, str]);
 };
 
 exports.skataAlert = function(mac, str, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'EpsilonPrinter', 'skataAlert', [mac, str]);
+    cordova.exec(successCallback, errorCallback, 'ZebraBluetoothPrinter', 'skataAlert', [mac, str]);
 };
