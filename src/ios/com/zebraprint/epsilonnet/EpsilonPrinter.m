cd1194a0b70541e6c6b29babe53b51ecded50744
bicossampalis <beaucoupfish25@hotmail.com>
2016-07-13 12:10:44 +0300

    2,31

--- src/ios/com/zebraprint/epsilonnet/EpsilonPrinter.m
+++ src/ios/com/zebraprint/epsilonnet/EpsilonPrinter.m
@@ -1,16 +1,17 @@
 #import "EpsilonPrinter.h"
-
+#import <UIKit/UIKit.h>
+#import <Cordova/NSArray+Comparisons.h>
+#import <Cordova/NSDictionary+Extensions.h>
  
   @implementation EpsilonPrinter
-  @synthesize cordova_command;
+
   
 
  
  -(void)skataAlert:(CDVInvokedUrlCommand*)command{
  
- 
- 
-   [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"skata"] callbackId:self.cordova_command.callbackId];
+   CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsInt:command.callbackId];
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 
 
