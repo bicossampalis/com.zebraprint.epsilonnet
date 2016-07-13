bd4fcfffc0dc403d8c4a2cc7042d51b62f7c3502
bicossampalis <beaucoupfish25@hotmail.com>
2016-07-13 15:10:37 +0300

    2.41

--- src/ios/com/zebraprint/epsilonnet/MfiBtPrinterConnection.m
+++ src/ios/com/zebraprint/epsilonnet/MfiBtPrinterConnection.m
@@ -15,26 +15,16 @@
              //Note: This will find the first printer connected! If you have multiple Zebra printers connected, you should display a list to the user and have him select the one they wish to use
          }
      }
-     // Instantiate connection to Zebra Bluetooth accessory
-     id<ZebraPrinterConnection, NSObject> thePrinterConn = [[MfiBtPrinterConnection alloc] initWithSerialNumber:serialNumber];
      
-     // Open the connection - physical connection is established here.
-     BOOL success = [thePrinterConn open];
      
      // This example prints "This is a ZPL test." near the top of the label.
      NSString *zplData = [command objectAtIndex:1];
      
      NSError *error = nil;
      // Send the data to printer as a byte array.
-     success = success && [thePrinterConn write:[zplData dataUsingEncoding:NSUTF8StringEncoding] error:&error];
+
      bool bIsError = FALSE;
-     if (success != YES || error != nil) {
-       bIsError = TRUE;
-     }
-     // Close the connection to release resources.
-     [thePrinterConn close];
-     
-     //[thePrinterConn release];
+
 	 
 	  return bIsError;
  }
