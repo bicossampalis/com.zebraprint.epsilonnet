25c5232386ed3aa57c150a2fa57aece39fd33153
bicossampalis <beaucoupfish25@hotmail.com>
2016-07-13 15:02:23 +0300

    2.39

--- src/ios/com/zebraprint/epsilonnet/MfiBtPrinterConnection.m
+++ src/ios/com/zebraprint/epsilonnet/MfiBtPrinterConnection.m
@@ -1,11 +1,20 @@
 #import <ExternalAccessory/ExternalAccessory.h>
- #import "MfiBtPrinterConnection.h"
+#import "MfiBtPrinterConnection.h"
  
   @implementation MfiBtPrinterConnection
  -(bool)sendZplOverBluetoothParent:(NSArray*)command{
  
-     NSString *serialNumber = [command objectAtIndex:0];
-     
+         NSString *serialNumber = @"";
+     //Find the Zebra Bluetooth Accessory
+     EAAccessoryManager *sam = [EAAccessoryManager sharedAccessoryManager];
+     NSArray * connectedAccessories = [sam connectedAccessories];
+     for (EAAccessory *accessory in connectedAccessories) {
+         if([accessory.protocolStrings indexOfObject:@"com.zebra.rawport"] != NSNotFound){
+             serialNumber = accessory.serialNumber;
+             break;
+             //Note: This will find the first printer connected! If you have multiple Zebra printers connected, you should display a list to the user and have him select the one they wish to use
+         }
+     }
      // Instantiate connection to Zebra Bluetooth accessory
      id<ZebraPrinterConnection, NSObject> thePrinterConn = [[MfiBtPrinterConnection alloc] initWithSerialNumber:serialNumber];
      
