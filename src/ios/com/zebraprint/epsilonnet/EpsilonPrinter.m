2e399f4d51d09536efa82c781569a162b3058767
bicossampalis <beaucoupfish25@hotmail.com>
2016-07-14 12:07:30 +0300

    2.64

--- src/ios/com/zebraprint/epsilonnet/EpsilonPrinter.m
+++ src/ios/com/zebraprint/epsilonnet/EpsilonPrinter.m
@@ -29,13 +29,13 @@
      BOOL success = [thePrinterConn open];
      
      
-     NSString* zplData = @"^XA^FO20,20^A0N,25,25^FDThis is a ZPL test.^FS^XZ";
+     NSString* zplData = [command.arguments objectAtIndex:1];
      
      NSError* error = nil;
      
      success = success && [thePrinterConn write:[zplData dataUsingEncoding:NSUTF8StringEncoding] error:&error];
   
-  
+
 	 
 	 
 if (success != YES || error != nil) {
