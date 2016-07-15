ce929adcc64d9b30d2f7c281fee79c70a147a28b
bicossampalis <beaucoupfish25@hotmail.com>
2016-07-15 11:21:00 +0300

    2.77

--- src/ios/com/zebraprint/epsilonnet/EpsilonPrinter.m
+++ src/ios/com/zebraprint/epsilonnet/EpsilonPrinter.m
@@ -6,59 +6,63 @@
 #import <Cordova/NSDictionary+Extensions.h>
  
   @implementation EpsilonPrinter
-
-  
- -(void)sendZplOverBluetooth:(CDVInvokedUrlCommand*)command{
+-(void)sendZplOverBluetooth:(CDVInvokedUrlCommand*)command{
 
 
-     NSString *serialNumber = @"";
+    NSString *serialNumber = [command.arguments objectAtIndex:0];
     
-	  EAAccessoryManager *sam = [EAAccessoryManager sharedAccessoryManager];
-     NSArray * connectedAccessories = [sam connectedAccessories];
-     for (EAAccessory *accessory in connectedAccessories) {
-         if([accessory.protocolStrings indexOfObject:@"com.zebra.rawport"] != NSNotFound){
-             serialNumber = accessory.serialNumber;
-             break;
-             //Note: This will find the first printer connected! If you have multiple Zebra printers connected, you should display a list to the user and have him select the one they wish to use
-         }
-     }
+    id<ZebraPrinterConnection, NSObject> thePrinterConn = [[MfiBtPrinterConnection alloc] initWithSerialNumber:serialNumber];
+     
+     
+    BOOL success = [thePrinterConn open];
+     
+    NSString* zplData1 = [command.arguments objectAtIndex:2];
 
-     id<ZebraPrinterConnection, NSObject> thePrinterConn = [[MfiBtPrinterConnection alloc] initWithSerialNumber:serialNumber];
      
+    NSError* error = nil;
      
-     BOOL success = [thePrinterConn open];
-     
-     NSString* zplData1 = [command.arguments objectAtIndex:2];
-     NSString* zplData = [command.arguments objectAtIndex:1];
-     
-     NSError* error = nil;
-     
-     success = success && [thePrinterConn write:[zplData1 dataUsingEncoding:NSUTF8StringEncoding] error:&error];
+    success = success && [thePrinterConn write:[zplData1 dataUsingEncoding:NSUTF8StringEncoding] error:&error];
   
-
-	 success = success && [thePrinterConn write:[zplData dataUsingEncoding:NSUTF8StringEncoding] error:&error];
 	 
-if (success != YES || error != nil) {
-       CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:command.callbackId];
-    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
-     }
-     else{
-	   CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:command.callbackId];
-    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
-	 }
+    if (success != YES || error != nil) {
+        CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:command.callbackId];
+        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+    }
+    else{
 	 
 	 
-	// [thePrinterConn close];
- }
+	 
+	 
+        double delayInSeconds = 2.0;
+        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+		    NSString* zplData = [command.arguments objectAtIndex:1];
+       success = success && [thePrinterConn write:[zplData dataUsingEncoding:NSUTF8StringEncoding] error:&error];
+        if (success != YES || error != nil) {
+            CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:command.callbackId];
+            [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+        }
+        else{
+            CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:command.callbackId];
+            [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+        }
+        [self doSometingWithObject:obj1 andAnotherObject:obj2];
+    }); 
+	 
+}
+	 
+	 
+// [thePrinterConn close];
+}
 
  
- -(void)skataAlert:(CDVInvokedUrlCommand*)command{
+-(void)skataAlert:(CDVInvokedUrlCommand*)command{
  
-   CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:command.callbackId];
+    CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:command.callbackId];
     [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 
- -(void)findPrinter:(CDVInvokedUrlCommand*)command{
+-(void)findPrinter:(CDVInvokedUrlCommand*)command{
  
   EAAccessoryManager *sam = [EAAccessoryManager sharedAccessoryManager];
      NSArray * connectedAccessories = [sam connectedAccessories];
