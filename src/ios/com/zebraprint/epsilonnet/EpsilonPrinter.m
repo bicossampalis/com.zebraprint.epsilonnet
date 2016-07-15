13199e84e1f6e9e676ad22fb3dbaf34973e36529
bicossampalis <beaucoupfish25@hotmail.com>
2016-07-15 11:47:02 +0300

    2.79

--- src/ios/com/zebraprint/epsilonnet/EpsilonPrinter.m
+++ src/ios/com/zebraprint/epsilonnet/EpsilonPrinter.m
@@ -11,18 +11,9 @@
  -(void)sendZplOverBluetooth:(CDVInvokedUrlCommand*)command{
 
 
-     NSString *serialNumber = @"";
+     NSString *serialNumber =[command.arguments objectAtIndex:0];
     
-	  EAAccessoryManager *sam = [EAAccessoryManager sharedAccessoryManager];
-     NSArray * connectedAccessories = [sam connectedAccessories];
-     for (EAAccessory *accessory in connectedAccessories) {
-         if([accessory.protocolStrings indexOfObject:@"com.zebra.rawport"] != NSNotFound){
-             serialNumber = accessory.serialNumber;
-             break;
-             //Note: This will find the first printer connected! If you have multiple Zebra printers connected, you should display a list to the user and have him select the one they wish to use
-         }
-     }
-
+	
      id<ZebraPrinterConnection, NSObject> thePrinterConn = [[MfiBtPrinterConnection alloc] initWithSerialNumber:serialNumber];
      
      
