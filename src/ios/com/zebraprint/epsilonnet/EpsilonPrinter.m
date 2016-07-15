c9b5cc3674deea9ad422cd4a8abd15538045dd4a
bicossampalis <beaucoupfish25@hotmail.com>
2016-07-15 12:38:23 +0300

    2.85

--- src/ios/com/zebraprint/epsilonnet/EpsilonPrinter.m
+++ src/ios/com/zebraprint/epsilonnet/EpsilonPrinter.m
@@ -11,12 +11,12 @@
  -(void)changeLanguage:(CDVInvokedUrlCommand*)command{
 
 
-     NSString *serialNumber = [command.arguments objectAtIndex:0];
+     NSString *serialNumber = @"";
 
 EAAccessoryManager *sam = [EAAccessoryManager sharedAccessoryManager];
      NSArray * connectedAccessories = [sam connectedAccessories];
      for (EAAccessory *accessory in connectedAccessories) {
-         if([accessory.protocolStrings indexOfObject:@"com.zebra.rawport"] != NSNotFound && accessory.serialNumber == serialNumber){
+         if([accessory.protocolStrings indexOfObject:@"com.zebra.rawport"] != NSNotFound){
              serialNumber = accessory.serialNumber;
              break;
              //Note: This will find the first printer connected! If you have multiple Zebra printers connected, you should display a list to the user and have him select the one they wish to use
