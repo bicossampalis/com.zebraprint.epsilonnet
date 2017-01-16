32290692c058fa00da395d9dd8d96d3b5ec2da59
bicossampalis <beaucoupfish25@hotmail.com>
2017-01-16 17:15:20 +0200

    3.31

--- src/ios/com/zebraprint/epsilonnet/PrinterStatus.h
+++ src/ios/com/zebraprint/epsilonnet/PrinterStatus.h
@@ -19,7 +19,7 @@
  *  Get the current status of a printer.
  *
  *  \code
-#import "TcpPrinterConnection.h"
+#import "ZebraPrinterConnection.h"
 #import "ZebraPrinterFactory.h"
 #import "ZebraPrinter.h"
 #import "PrinterStatus.h"
