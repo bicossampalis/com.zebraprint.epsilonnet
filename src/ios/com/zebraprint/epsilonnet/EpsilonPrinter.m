2a3ef2beb75969f0655959b704b1009ebf49c5c6
bicossampalis <beaucoupfish25@hotmail.com>
2017-01-16 16:42:04 +0200

    3.30

--- src/ios/com/zebraprint/epsilonnet/EpsilonPrinter.m
+++ src/ios/com/zebraprint/epsilonnet/EpsilonPrinter.m
@@ -2,10 +2,9 @@
 #import "MfiBtPrinterConnection.h"
 #import <ExternalAccessory/ExternalAccessory.h>
 #import <UIKit/UIKit.h>
-//#import "PrinterStatus.h"
-//#import "ZebraPrinter.h"
 #import "ZebraPrinterConnection.h"
 #import "PrinterStatus.h"
+#import "ZebraPrinterFactory.h"
 #import "ZebraPrinter.h"
  
   @implementation EpsilonPrinter
